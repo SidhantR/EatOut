a877e6b5d66ceea62a38ff5d3b56b8d8c5375f7e
SidhantR <sidhantrc02@gmail.com>
2024-02-15 22:36:27 +0530

    home page

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <div className="footer">
+      Created By
+      <a href="">Sidhant singh</a>
+      <strong>Eat Out</strong>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="logo"
+        alt="logo"
+        src="https://i.pinimg.com/564x/da/37/88/da3788cc1317499525c3784bb2fa578f.jpg"
+      />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header
