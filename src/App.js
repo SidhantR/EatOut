f95f04db9a9e106974ff6ffa3a9c8b0ca0d5ead4
SidhantR <sidhantrc02@gmail.com>
2024-02-25 23:18:53 +0530

    movin data to constant

--- src/App.js
+++ src/App.js
@@ -15,5 +15,4 @@ const AppLayout = () => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(<AppLayout />);
---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,27 @@
+import {IMG_CDN_URL} from '../constant'
+
+const RestaurantCard = ({
+    cloudinaryImageId,
+    name,
+    cuisines,
+    area,
+    lastMileTravelString,
+    costForTwoString,
+    avgRating,
+  }) => {
+    return (
+      <div className="card">
+        <img
+          src={
+            IMG_CDN_URL +
+            cloudinaryImageId
+          }
+        />
+        <h2>{name}</h2>
+        <h3>{cuisines.join(", ")}</h3>
+        <h4>{area} stars</h4>
+      </div>
+    );
+  };
+
+export default RestaurantCard
